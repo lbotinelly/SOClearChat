c5f44aa77c827f78018be97fce8e493e7dd968f7
Guilherme Nascimento <brcontainer@yahoo.com.br>
2016-11-26 12:10:50 -0200

    Update so-clearchat.js

--- so-clearchat.js
+++ so-clearchat.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SO ClearChat
 // @namespace    com.onosendai
-// @version      0.3.2
+// @version      0.3.3
 // @author       OnoSendai
 // @match        http://chat.stackexchange.com/rooms/*
 // @match        https://chat.stackexchange.com/rooms/*
@@ -11,66 +11,75 @@
 
 setTimeout(function(){
     console.log('Hooking up!');
-    
-    $(function(){
-        var r= $('<a title="Clear this chat window" target="_self" id="soc_clr" style="cursor:pointer;">clear</a> | ' + 
-                 '<a title="Clear this chat window" target="_self" id="soc_wipeimg" style="cursor:pointer;">remove media</a>' + 
+
+    $(function () {
+        var r = $('<a title="Clear this chat window" href="javascript:void(0);" id="soc_clr">clear</a> | ' +
+                 '<a title="Clear this chat window" href="javascript:void(0);" id="soc_wipeimg">remove media</a>' +
                  '<span> | </span>');
+
         $("#sidebar-menu").prepend(r);
 
-
         $("#soc_clr").click(function (e) {
-            $("div.monologue").remove();
-            $("div.system-message").remove();
-            event.preventDefault();
+            e.preventDefault();
+
+            $("div.monologue, div.system-message").remove();
         });
 
-        $("#soc_wipeimg").click(function (e) {
+        $("#soc_wipeimg").click(function(e) {
+            e.preventDefault();
+
             $("div.ob-image > a").addClass('deleted').text('(Image removed)');
             $("div.ob-youtube > a").addClass('deleted').text('(Video removed)');
             $("div.monologue .messages:contains('▒')").text('(Silly ASCII art removed)');
         });
 
         // Favorites image link-to-thumbnail monitor
-        var sheet = (function() {
-            var style = document.createElement("style");
-            style.appendChild(document.createTextNode(""));
-            document.head.appendChild(style);
-            return style.sheet;
-        })();
+        var style = document.createElement("style");
 
+        rules = '.hoverable {' +
+                '   display: none;' +
+                '   position: absolute;' +
+                '   z-index: 1;' +
+                '   left: -142px;' +
+                '   top:-1px;' +
+                '   border: 4px solid white;' +
+                '   width:128px;' +
+                '} ' +
+                'a:hover .hoverable {' +
+                '   display: block;' +
+                '}';
 
-        sheet.insertRule(".hoverable { display: none; position: absolute; z-index: 1; left: -142px; top:-1px; border: 4px solid white; width:128px;}", 0);
-        sheet.insertRule("a:hover .hoverable { display: block; }", 0);
-        
-        function doThumbnails(){
-            
-            var elms = $('#starred-posts .collapsible a[href$=".gif"], #starred-posts .collapsible a[href$=".jpg"], #starred-posts .collapsible a[href$=".png"]')
-            .filter(':not(.thumbified)');
-            
-            console.log(elms.size());
-            
-            elms            
-                .each(function() {
-                
-            console.log(this.href);
-                
+        if (style.styleSheet) {
+            style.styleSheet.cssText = rules;
+        } else {
+            style.appendChild(document.createTextNode(rules));
+        }
 
-                $(this).addClass('thumbified');
-                
-                var img = $('<img>',{src: this.href, class:'hoverable'});
-                
-                $(this).append(img);
+        document.head.appendChild(style);
+
+        function doThumbnails()
+        {
+            var elms = $('#starred-posts .collapsible a[href$=".gif"],' +
+                         '#starred-posts .collapsible a[href$=".jpeg"],' +
+                         '#starred-posts .collapsible a[href$=".jpg"],' +
+                         '#starred-posts .collapsible a[href$=".png"]');
+
+            elms = elms.filter(':not(.thumbified)');
+
+            elms.each(function() {
+                var img = $('<img>', { 'src': this.href, 'class': 'hoverable' });
+
+                $(this).addClass('thumbified').append(img);
             });
-        };
+        }
 
-        $("#starred-posts .collapsible").bind("DOMNodeInserted",function(){
-            doThumbnails();    
+        new MutationObserver(function(e) {
+            doThumbnails();
+        }).observe($("#starred-posts .collapsible").get(0), {
+            childList: true,
+            subtree: true
         });
 
         doThumbnails();
-
     });
 }, 1000);
-
-// "Para funcionar no IE, baixe o Chrome" - emanuelsn
